4fd8e53cd54331f2516ffb298e3fa6659c2a93da
dev-HongCheol <choisieun89@gmail.com>
2024-04-25 23:16:06 +0900

    Fix: 모달 닫기 수정

--- src/app/(afterLogin)/_component/composeTweet/ComposeTweet.tsx
+++ src/app/(afterLogin)/_component/composeTweet/ComposeTweet.tsx
@@ -8,14 +8,16 @@ import ve from "./composeTweet.css";
 import Link from "next/link";
 import MediaUpload from "@/components/icons/MediaUpload";
 import GifUpload from "@/components/icons/GifUpload";
+import { useRouter } from "next/navigation";
 
 // FIXME: TESTCODE
 const me = {
   src: "/default_profile_normal.png",
 };
-const ComposeTweet = () => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
+const ComposeTweet = () => {
+  const router = useRouter();
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   const mediaInput = useRef<HTMLInputElement>(null);
   const handleUploadMedia = () => {
     mediaInput.current?.click();
@@ -26,7 +28,7 @@ const ComposeTweet = () => {
     console.log("🚀  file: Login.tsx:21  onSubmit  event_", event);
     console.log("1111", textareaRef.current?.value, textareaRef.current?.value);
   };
-  const [isShow, setIsShow] = useState(true);
+
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -36,88 +38,83 @@ const ComposeTweet = () => {
     setIsSubmitDisabled(event.target.value.length === 0);
   };
 
-  const onClose = () => setIsShow((pre) => !pre);
-  function adjustTextareaHeight() {
+  const onClose = () => router.back();
+
+  const adjustTextareaHeight = () => {
     const textarea = textareaRef.current;
     if (!textarea) return;
 
     textarea.style.height = "auto"; // Reset height to auto to recalculate
     textarea.style.height = `${textarea.scrollHeight}px`; // Set height to scrollHeight
-  }
+  };
 
   return (
-    isShow && (
-      <Modal className={ve.modalWrapper}>
-        {/* header */}
-        <div className={ve.modalHeader}>
-          <Button variant="text" className={ve.closeBtn} onClick={onClose}>
-            <Image src={closeBtnImg} alt="close btn" />
-          </Button>
-        </div>
+    <Modal className={ve.modalWrapper}>
+      {/* header */}
+      <div className={ve.modalHeader}>
+        <Button variant="text" className={ve.closeBtn} onClick={onClose}>
+          <Image src={closeBtnImg} alt="close btn" />
+        </Button>
+      </div>
 
-        <form className={ve.tweetForm} onSubmit={onSubmit}>
-          <div className={ve.contextWrapper}>
-            <div className={ve.avatarImageWrapper}>
-              <Image
-                src={me.src}
-                alt="profileImage"
-                fill
-                className={ve.avatarImage}
-              />
-            </div>
-            <div className={ve.textareaWrapper}>
-              <textarea
-                className={ve.formTextarea}
-                placeholder="무슨 일이 일어나고 있나요?"
-                onInput={adjustTextareaHeight}
-                ref={textareaRef}
-                onChange={onChangeTextarea}
-              />
-            </div>
+      <form className={ve.tweetForm} onSubmit={onSubmit}>
+        <div className={ve.contextWrapper}>
+          <div className={ve.avatarImageWrapper}>
+            <Image
+              src={me.src}
+              alt="profileImage"
+              fill
+              className={ve.avatarImage}
+            />
           </div>
-          <span>
-            <Link href={"#"}>모든 사람에게 보일 수 있습니다.</Link>
-          </span>
-          <hr className={ve.hr} />
-          <div className={ve.actionButtons}>
-            <div className={ve.uploadContentBtnWrapper}>
-              <Button
-                variant="text"
-                className={ve.contentUploadBtn}
-                title="미디어"
-                onClick={handleUploadMedia}
-              >
-                <MediaUpload />
-              </Button>
-              <input
-                type="file"
-                hidden
-                name="media"
-                multiple
-                accept="image/*"
-                ref={mediaInput}
-              />
-              <Button
-                variant="text"
-                className={ve.contentUploadBtn}
-                title="GIF"
-              >
-                <GifUpload />
-              </Button>
-            </div>
-            <div>
-              <Button
-                className={ve.submitButton}
-                type="submit"
-                disabled={isSubmitDisabled}
-              >
-                게시하기
-              </Button>
-            </div>
+          <div className={ve.textareaWrapper}>
+            <textarea
+              className={ve.formTextarea}
+              placeholder="무슨 일이 일어나고 있나요?"
+              onInput={adjustTextareaHeight}
+              ref={textareaRef}
+              onChange={onChangeTextarea}
+            />
           </div>
-        </form>
-      </Modal>
-    )
+        </div>
+        <span>
+          <Link href={"#"}>모든 사람에게 보일 수 있습니다.</Link>
+        </span>
+        <hr className={ve.hr} />
+        <div className={ve.actionButtons}>
+          <div className={ve.uploadContentBtnWrapper}>
+            <Button
+              variant="text"
+              className={ve.contentUploadBtn}
+              title="미디어"
+              onClick={handleUploadMedia}
+            >
+              <MediaUpload />
+            </Button>
+            <input
+              type="file"
+              hidden
+              name="media"
+              multiple
+              accept="image/*"
+              ref={mediaInput}
+            />
+            <Button variant="text" className={ve.contentUploadBtn} title="GIF">
+              <GifUpload />
+            </Button>
+          </div>
+          <div>
+            <Button
+              className={ve.submitButton}
+              type="submit"
+              disabled={isSubmitDisabled}
+            >
+              게시하기
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Modal>
   );
 };
 
--- src/app/(beforeLogin)/_component/login/Login.tsx
+++ src/app/(beforeLogin)/_component/login/Login.tsx
@@ -13,7 +13,10 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.svg";
 import Button from "@/components/uis/atoms/Button";
 import Input from "@/components/uis/atoms/Input";
+import { useRouter } from "next/navigation";
+
 const Login = () => {
+  const router = useRouter();
   const textRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
@@ -22,55 +25,49 @@ const Login = () => {
     console.log("🚀  file: Login.tsx:21  onSubmit  event_", event);
     console.log("1111", textRef.current?.value, passwordRef.current?.value);
   };
-  const [isShow, setIsShow] = useState(true);
 
-  useEffect(() => {
-    console.log("🚀  file: Login.tsx:26  Login  isShow_", isShow);
-  }, [isShow]);
-  /* FIXME: 모달을 자주 사용하면 외부로 뺴자. */
-  const onClose = () => setIsShow((pre) => !pre);
+  const onClose = () => router.back();
+
   return (
-    isShow && (
-      <Modal>
-        {/* header */}
-        <div className={modalHeader}>
-          <div className={headerFlex}>
-            <Button variant="text" className={closeBtn} onClick={onClose}>
-              <Image src={closeBtnImg} alt="close btn" />
-            </Button>
-          </div>
-          <div>
-            <Image
-              src={logoImg}
-              alt="logo"
-              width={30}
-              height={53}
-              className={logoImage}
-            />
-          </div>
-          <div className={headerFlex}></div>
-        </div>
-
-        {/* title */}
-        <form className={loginForm} onSubmit={onSubmit}>
-          <h2>X 가입하기</h2>
-          {/* <Button variant="outlined">Google 계정으로 로그인</Button>
-        <Button variant="outlined">Apple로 로그인</Button> */}
-          <br />
-          <Input id="id" name="id" label="아이디" ref={textRef} />
-          <Input
-            id="password"
-            name="password"
-            label="비밀번호"
-            type="password"
-            ref={passwordRef}
-          />
-          <Button variant="contained" type="submit">
-            로그인하기
+    <Modal>
+      {/* header */}
+      <div className={modalHeader}>
+        <div className={headerFlex}>
+          <Button variant="text" className={closeBtn} onClick={onClose}>
+            <Image src={closeBtnImg} alt="close btn" />
           </Button>
-        </form>
-      </Modal>
-    )
+        </div>
+        <div>
+          <Image
+            src={logoImg}
+            alt="logo"
+            width={30}
+            height={53}
+            className={logoImage}
+          />
+        </div>
+        <div className={headerFlex}></div>
+      </div>
+
+      {/* title */}
+      <form className={loginForm} onSubmit={onSubmit}>
+        <h2>X 가입하기</h2>
+        {/* <Button variant="outlined">Google 계정으로 로그인</Button>
+        <Button variant="outlined">Apple로 로그인</Button> */}
+        <br />
+        <Input id="id" name="id" label="아이디" ref={textRef} />
+        <Input
+          id="password"
+          name="password"
+          label="비밀번호"
+          type="password"
+          ref={passwordRef}
+        />
+        <Button variant="contained" type="submit">
+          로그인하기
+        </Button>
+      </form>
+    </Modal>
   );
 };
 
