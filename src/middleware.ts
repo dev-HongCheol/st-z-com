fc6ef181afd0f09b4290fd71f1437263da660165
dev-HongCheol <choisieun89@gmail.com>
2024-06-24 13:10:24 +0900

    fix: 로그인 체크 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
+import { cookies } from "next/headers";
 
 export const middleware = async () => {
-  const session = await auth();
+	const session = await auth();
+	const cookie = cookies();
 
-  if (!session) {
-    return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_DOMAIN}/i/flow/login`
-    );
-  }
+	if (!session && !cookie.get("connect.sid")) {
+		return NextResponse.redirect(`${process.env.NEXT_PUBLIC_DOMAIN}/i/flow/login`);
+	}
 };
 
 export const config = {
-  matcher: ["/compose/tweet", "/home", "/explore", "/message", "/search"],
+	matcher: ["/compose/tweet", "/home", "/explore", "/message", "/search"],
 };
